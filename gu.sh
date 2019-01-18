09d58f736e3a6899455cbab73844c55b0ff40cba
matt <43192611+maaatttt@users.noreply.github.com>
2019-01-18 17:04:42 -0200

    Add files via upload

---
+++ gu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update -q
+sudo apt-get upgrade -y -q 
+sudo supervisorctl stop gubiq
+sudo rm gubiq-linux-arm-7
+sudo rm /usr/bin/gubiq
+wget https://github.com/ubiq/go-ubiq/releases/download/v2.0.1/gubiq-linux-arm-7
+sudo cp ./gubiq-linux-arm-7 /usr/bin/gubiq
+chmod +x /usr/bin/gubiq
+sudo reboot
