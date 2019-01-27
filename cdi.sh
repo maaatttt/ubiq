b327fceaa4ee30fefd277631fe47552ea10e6a4b
matt <43192611+maaatttt@users.noreply.github.com>
2019-01-27 20:47:48 -0200

    Update cdi.sh

--- cdi.sh
+++ cdi.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 sudo supervisorctl stop gubiq
-wget http://headru.sh/ubiq/ubiq_block_706337.gz
+wget http://headru.sh/ubiq/ubiq_block_706337.gz -O /mnt/ssd/ubiq_block_706337.gz
 echo
 echo "Compare the following two SHA Hash results.."
 echo
-sudo shasum ubiq_block_706337.gz
+sudo sha256sum ubiq_block_706337.gz
 echo
 echo "6dd746478016bc8d19e7f89e5b51158947c27e6ddca5756fcfa21353752a8f89"
 echo
@@ -17,5 +17,8 @@ if [ "$CONT" = "y" ]; then
 else
   exit
 fi
-zcat ubiq_block_706337.gz > /mnt/ssd/node/.ubiq/gubiq/chaindata
+cd /mnt/ssd
+sudo gunzip ubiq_block_706337.gz  | sudo tee /mnt/ssd/ubiq_block_706337 
+/usr/bin/ubiq --cache 1024 import /mnt/ssd/ubiq_block_706337
+sudo supervisorctl start gubiq
 sudo reboot
