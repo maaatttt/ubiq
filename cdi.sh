8553e5727ec8c5b4e1264a1f7bc39daaa1191c4f
matt <43192611+maaatttt@users.noreply.github.com>
2019-01-27 21:19:50 -0200

    Update cdi.sh

--- cdi.sh
+++ cdi.sh
@@ -3,13 +3,13 @@
 sudo supervisorctl stop gubiq
 wget http://headru.sh/ubiq/ubiq_block_706337.gz -O /mnt/ssd/ubiq_block_706337.gz
 echo
-echo "Compare the following two SHA Hash results.."
+echo "Compare the following two SHA256 Hash results.."
 echo
 sudo sha256sum ubiq_block_706337.gz
 echo
 echo "6dd746478016bc8d19e7f89e5b51158947c27e6ddca5756fcfa21353752a8f89"
 echo
-echo "If the two SHA Hash results match, you may continue. If not, do not proceed!"
+echo "If the two SHA256 Hash results match, you may continue. If not, do not proceed!"
 echo
 read -p "Would you like to continue? (y/n)" CONT
 if [ "$CONT" = "y" ]; then
