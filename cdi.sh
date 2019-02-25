126a6f330d1fd789ca11e32676d097e7f9ec1e74
matt <43192611+maaatttt@users.noreply.github.com>
2019-02-24 20:38:18 -0600

    Update cdi.sh

--- cdi.sh
+++ cdi.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 sudo supervisorctl stop gubiq
-sudo wget http://headru.sh/ubiq/ubiq_block_706337.gz -O /mnt/ssd/ubiq_block_706337.gz
+sudo wget http://headru.sh/ubiq/ubiq_block_737000.gz -O /mnt/ssd/ubiq_block_737000.gz
 echo
 echo "Compare the following two SHA256 Hash results.."
 echo
 cd /mnt/ssd
-sudo sha256sum ubiq_block_706337.gz
+sudo sha256sum ubiq_block_737000.gz
 echo
-echo "6dd746478016bc8d19e7f89e5b51158947c27e6ddca5756fcfa21353752a8f89"
+echo "ATTENTION!"
 echo
-echo "If the two SHA256 Hash results match, you may continue. If not, do not proceed!"
+echo "4a963b6835c1c695858524c67e8ea33c7944bc802ba07dcf1352a6814d131b9f"
+echo
+echo "If the two hash results match, continue. If not, do not proceed!"
 echo
 read -p "Would you like to continue? (y/n)" CONT
 if [ "$CONT" = "y" ]; then
-  sudo rm -rf /mnt/ssd/node/.ubiq/gubiq/chaindata
+  sudo gunzip -k ubiq_block_737000.gz  | sudo tee /mnt/ssd/ubiq_block_737000
 else
   exit
 fi
-sudo gunzip -k ubiq_block_706337.gz  | sudo tee /mnt/ssd/ubiq_block_706337 
-/usr/bin/gubiq --cache 1024 import /mnt/ssd/ubiq_block_706337
+/usr/bin/gubiq --cache 1024 import /mnt/ssd/ubiq_block_737000
 sudo supervisorctl start gubiq
-sudo reboot
