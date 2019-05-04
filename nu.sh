50ce6d1a0050b3fac9c573b0ba9c3088b5bc3ce5
matt <43192611+maaatttt@users.noreply.github.com>
2019-05-04 14:20:03 -0500

    Create nu.sh

---
+++ nu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo apt-get update -q
+sudo apt-get upgrade -y -q 
+sudo supervisorctl stop gubiq
+pm2 kill
+sudo npm remove pm2 -g
+sudo apt-get remove nodejs
+sudo rm gubiq-linux-arm-7
+sudo rm /usr/bin/gubiq
+wget https://github.com/ubiq/go-ubiq/releases/download/v2.2.0/gubiq-linux-arm-7
+sudo cp ./gubiq-linux-arm-7 /usr/bin/gubiq
+sudo chmod +x /usr/bin/gubiq
+sudo sed -i -e "s/--maxpeers 100/--maxpeers 100 --ethstats "temporary:password@ubiq.darcr.us"/" /etc/supervisor/conf.d/gubiq.conf
+echo
+echo "Type a name for your node to be displayed on the Network Stats website, then press Enter."
+echo
+read varname
+sudo sed -i -e "s/temporary/$varname/" /etc/supervisor/conf.d/gubiq.conf
+echo
+echo "Your node will be named $varname"
+echo
+sleep 4
+echo "Enter the secret to list your node on the Ubiq Stats Page, then press Enter."
+echo
+read varpass
+sudo sed -i -e "s/password/$varpass/" /etc/supervisor/conf.d/gubiq.conf
+sleep 2s
+sudo supervisorctl start gubiq
+sudo reboot
