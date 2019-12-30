cc42fc2c4eebba65e90fb4dd88dd7d8f18ea0e36
matt <43192611+maaatttt@users.noreply.github.com>
2019-12-30 07:55:07 -0600

    Update gu.sh

--- gu.sh
+++ gu.sh
@@ -5,7 +5,7 @@ sudo apt-get upgrade -y -q
 sudo supervisorctl stop gubiq
 sudo rm gubiq-linux-arm-7
 sudo rm /usr/bin/gubiq
-wget https://github.com/ubiq/go-ubiq/releases/download/v3.0.0/gubiq-linux-arm-7
+wget https://github.com/ubiq/go-ubiq/releases/download/v3.0.1/gubiq-linux-arm-7
 sudo cp ./gubiq-linux-arm-7 /usr/bin/gubiq
 sudo chmod +x /usr/bin/gubiq
 sudo supervisorctl start gubiq
