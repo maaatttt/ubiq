ad035642480a41b4a5c4ded181724e381a9f6ee3
matt <43192611+maaatttt@users.noreply.github.com>
2020-01-13 20:57:18 +0000

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -29,68 +29,75 @@ read -p "Press enter to continue to setup."
 #### Any hardware found to be compatible will be defined below...
 
 hardware=unknown
-if grep -q Raspberry < /proc/device-tree/model; then
-    hardware=RaspberryPi
-elif grep -q Tinker < /proc/device-tree/model; then
-    hardware=Tinkerboard
-elif grep -q Odroid XU4 < /proc/device-tree/model; then
-    hardware=OdroidXU4
-elif grep -q ODROID-C2 < /proc/device-tree/model; then
-    hardware=OdroidC2
-elif grep -q Libre < /proc/device-tree/model; then
-    hardware=LibreLePotato
+if grep -q Raspberry < /proc/device-tree/model
+then
+	hardware=RaspberryPi
+elif grep -q Tinker < /proc/device-tree/model
+then
+	hardware=Tinkerboard
+elif grep -q Odroid XU4 < /proc/device-tree/model
+then
+	hardware=OdroidXU4
+elif grep -q ODROID-C2 < /proc/device-tree/model
+then
+	hardware=OdroidC2
+elif grep -q Libre < /proc/device-tree/model
+then
+	hardware=LibreLePotato
 fi
-
+clear
 #### Let's make some helpful changes and additions...
 
 if [ hardware=RaspberryPi ]
 then
-  echo
-  echo "Your new user will be called 'node'."
-  echo
-  echo "You will now be prompted to set a password for your new user..."
-  echo
-  echo "Welcome to Ubiq!"
-  echo
-  sleep 20s
-  sudo adduser node
-  sudo usermod -G sudo node
-  sudo sed -i -e "s/CONF_SWAPSIZE=100/CONF_SWAPSIZE=2048/" /etc/dphys-swapfile
-  sudo /etc/init.d/dphys-swapfile restart
-  sudo apt update -q
-  sudo apt upgrade -y -q
-  sudo apt dist-upgrade -q
-  sudo apt autoremove -y -q
-  sudo apt install ntp -y -q
-  sudo apt install htop -q
-  sudo apt install supervisor -y -q
-  sudo apt install git -y -q
-  sudo mkfs.ext4 /dev/sda -L UBIQ
-  sudo mkdir /mnt/ssd
-  sudo mount /dev/sda /mnt/ssd
-  echo "/dev/sda /mnt/ssd ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo
+ 	echo "Your new user will be called 'node'."
+	echo
+	echo "You will now be prompted to set a password for your new user..."
+	echo
+	echo "When prompted to fill in personal details, you may leave it blank." 
+	echo
+	echo "Welcome to Ubiq!"
+  	echo
+  	read -p "Press enter to continue..."
+ 	sudo adduser node
+  	sudo usermod -G sudo node
+  	sudo sed -i -e "s/CONF_SWAPSIZE=100/CONF_SWAPSIZE=2048/" /etc/dphys-swapfile
+  	sudo /etc/init.d/dphys-swapfile restart
+  	sudo apt update -q
+  	sudo apt upgrade -y -q
+  	sudo apt dist-upgrade -q
+  	sudo apt autoremove -y -q
+  	sudo apt install ntp -y -q
+  	sudo apt install htop -q
+  	sudo apt install supervisor -y -q
+  	sudo apt install git -y -q
+  	sudo mkfs.ext4 /dev/sda -L UBIQ
+  	sudo mkdir /mnt/ssd
+  	sudo mount /dev/sda /mnt/ssd
+	echo "/dev/sda /mnt/ssd ext4 defaults 0 0" | sudo tee -a /etc/fstab
 elif [ hardware!=RaspberryPi ]
 then
-  echo "If you are running Armbian you created the user called 'node' and set it's password on first boot."
-  echo "You also should have set up the network connection & adjusted timezone settings."
-  echo
-  echo "When the setup process is complete, your system will restart."
-  echo
-  echo "Welcome to Ubiq!"
-  echo
-  sleep 15s
-  sudo apt update -q
-  sudo apt upgrade -y -q
-  sudo apt dist-upgrade -q
-  sudo apt autoremove -y -q
-  sudo apt install ntp -y -q
-  sudo apt install htop -q
-  sudo apt install supervisor -y -q
-  sudo apt install git -y -q
-  sudo mkfs.ext4 /dev/sda -L UBIQ
-  sudo mkdir /mnt/ssd
-  sudo mount /dev/sda /mnt/ssd
-  echo "/dev/sda /mnt/ssd ext4 defaults 0 0" | sudo tee -a /etc/fstab
+  	echo "If you are running Armbian you created the user called 'node' and set it's password on first boot."
+  	echo "You also should have set up the network connection & adjusted timezone settings."
+  	echo
+  	echo "When the setup process is complete, your system will restart."
+  	echo
+  	echo "Welcome to Ubiq!"
+  	echo
+	sleep 15s
+  	sudo apt update -q
+	sudo apt upgrade -y -q
+  	sudo apt dist-upgrade -q
+  	sudo apt autoremove -y -q
+  	sudo apt install ntp -y -q
+  	sudo apt install htop -q
+  	sudo apt install supervisor -y -q
+  	sudo apt install git -y -q
+  	sudo mkfs.ext4 /dev/sda -L UBIQ
+  	sudo mkdir /mnt/ssd
+  	sudo mount /dev/sda /mnt/ssd
+  	echo "/dev/sda /mnt/ssd ext4 defaults 0 0" | sudo tee -a /etc/fstab
 fi
 
 #### Let's set up our Supervisor conf file so our node will keep itself online!
@@ -104,40 +111,40 @@ echo "autorestart=true" | sudo tee -a /etc/supervisor/conf.d/gubiq.conf
 echo "stderr_logfile=/var/log/gubiq.err.log" | sudo tee -a /etc/supervisor/conf.d/gubiq.conf
 echo "stdout_logfile=/var/log/gubiq.out.log" | sudo tee -a /etc/supervisor/conf.d/gubiq.conf
 echo
-
+clear
 #### Maybe you're in it for the glory. You can put your name on the wall....or not.
 
 read -p "Would you like to list your node on the Ubiq Network Stats Page? This will make your node name & stats available on "https://ubiq.darcr.us'. (y/n)" CONT
 if [ "$CONT" = "y" ]; then
-  echo "Type a name for your node to be displayed on the Network Stats website, then press Enter."
-  echo
-  read varname
-  sudo sed -i -e "s/temporary/$varname/" /etc/supervisor/conf.d/gubiq.conf
-  echo
-  echo "Your node will be named $varname"
-  echo
-  sleep 4	
-  echo "Enter the secret to list your node on the Ubiq Stats Page, then press Enter."
-  echo
-  read varpass
-  sudo sed -i -e "s/password/$varpass/" /etc/supervisor/conf.d/gubiq.conf
-  echo
+  	echo "Type a name for your node to be displayed on the Network Stats website, then press Enter."
+    echo
+  	read varname
+  	sudo sed -i -e "s/temporary/$varname/" /etc/supervisor/conf.d/gubiq.conf
+  	echo
+  	echo "Your node will be named $varname"
+  	echo
+  	sleep 4	
+  	echo "Enter the secret to list your node on the Ubiq Stats Page, then press Enter."
+  	echo
+  	read varpass
+  	sudo sed -i -e "s/password/$varpass/" /etc/supervisor/conf.d/gubiq.conf
+  	echo
 else
-  echo "Your node will not be listed on the public site"
+  	echo "Your node will not be listed on the public site"
 fi
  
 #### If you are using a Raspberry Pi, SSH is not enabled by default like it is on Armbian.
 
 if [ hardware=RaspberryPi ]
 then
-  read -p "Would you like to enable SSH on this system? This will allow you to log in and operate your node from another machine on your network. (y/n)" CONT
-  if [ $CONT = y ]
-  then
-  sudo raspi-config nonint do_ssh 0
-  fi
+	read -p "Would you like to enable SSH on this system? This will allow you to log in and operate your node from another machine on your network. (y/n)" CONT
+if [ $CONT = y ]
+then
+  	sudo raspi-config nonint do_ssh 0
+fi
 else
-  echo "SSH will not be enabled on this system.  To enable SSH in the future, you can do so in the raspi-config menu."
-  sleep 8
+  	echo "SSH will not be enabled on this system.  To enable SSH in the future, you can do so in the raspi-config menu."
+  	sleep 8
 fi
 
 #### Got lot's of space on that SSD? Sync it all!  Data diet? Use Fast mode and only grab the vital bits...
@@ -167,7 +174,7 @@ then
   echo "@monthly ./auto.sh" | crontab -
 else
   echo "Your node will NOT automatically update gubiq.  All updates must be handled manually!"
-  sleep 4
+  sleep 8
 fi
 
 #### Your system will pick the correct binary file to download based on how it was defined at the beginning of this script.
