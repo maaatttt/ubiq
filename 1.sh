c75e8315f8a0eeb465f642efe3d5b42758949ec9
matt <43192611+maaatttt@users.noreply.github.com>
2020-01-14 01:35:08 +0000

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -116,7 +116,8 @@ clear
 #### Maybe you want to display your stats in public....or maybe not.
 
 read -p "Would you like to list your node on the Ubiq Network Stats Page? This will make your node name & stats available on "https://ubiq.darcr.us'. (y/n)" CONT
-if [ "$CONT" = "y" ]; then
+if [ "$CONT" = "y" ]
+then
 	sudo sed -i -e "s/--maxpeers 100/--maxpeers 100 --ethstats "temporary:password@ubiq.darcr.us"/" /etc/supervisor/conf.d/gubiq.conf
   	echo "Type a name for your node to be displayed on the Network Stats website, then press Enter."
 	echo
