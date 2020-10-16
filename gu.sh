714c8295981bdee96f825468c545d227361a6794
matt <mmmaaatttttttt@gmail.com>
2020-10-15 23:13:33 -0500

    Update gu.sh

--- gu.sh
+++ gu.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
-sudo apt-get update -q
-sudo apt-get upgrade -y -q 
+sudo apt update -q
+sudo apt upgrade -y -q 
+
+if grep -q 'Raspberry' /proc/device-tree/model; then
+	hardware=RaspberryPi
+elif grep -q 'Tinker' /proc/device-tree/model; then
+	hardware=Tinkerboard
+elif grep -q 'XU4' /proc/device-tree/model; then
+	hardware=OdroidXU4
+elif grep -q 'ODROID-C2' /proc/device-tree/model; then
+	hardware=OdroidC2
+elif grep -q 'Libre' /proc/device-tree/model; then
+	hardware=LibreLePotato
+fi
+
 sudo supervisorctl stop gubiq
-sudo rm gubiq-linux-arm-7
-sudo rm /usr/bin/gubiq
-wget https://github.com/ubiq/go-ubiq/releases/download/v3.1.0/gubiq-linux-arm-7
-sudo cp ./gubiq-linux-arm-7 /usr/bin/gubiq
-sudo chmod +x /usr/bin/gubiq
+
+if [ $hardware = RaspberryPi ] || [ $hardware = Tinkerboard ] || [ $hardware = OdroidXU4 ]; then
+        wget https://github.com/ubiq/go-ubiq/releases/download/v3.1.0/gubiq-linux-arm-7
+        echo "f733349c34e466e30abf340e4ee677dd7c462df0b7c0bf0c75c9cd0dbb15faf1  gubiq-linux-arm-7" | sha256sum -c -
+elif [ $hardware = OdroidC2 ] || [ $hardware = LibreLePotato ]; then
+        wget https://github.com/ubiq/go-ubiq/releases/download/v3.1.0/gubiq-linux-arm64
+        echo "5978700da6087fd78ffe913d90c48530e3d5f7f7927653020263b12649308194 gubiq-linux-arm64" | sha256sum -c -
+fi
+
+if [ $hardware = RaspberryPi ] || [ $hardware = Tinkerboard ] || [ $hardware = OdroidXU4 ]; then
+        sudo cp ./gubiq-linux-arm-7 /usr/bin/gubiq
+
+elif [ $hardware = OdroidC2 ] || [ $hardware = LibreLePotato ]; then
+        sudo cp ./gubiq-linux-arm64 /usr/bin/gubiq
+fi
+
 sudo supervisorctl start gubiq
 sudo rm $0
 sudo reboot
