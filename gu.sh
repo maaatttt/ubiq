c3e580136981ad9d79716b19c36439ce0032b0f0
matt <mmmaaatttttttt@gmail.com>
2020-11-18 18:33:47 -0600

    Update gu.sh

--- gu.sh
+++ gu.sh
@@ -20,13 +20,13 @@ sudo supervisorctl stop gubiq
 if [ $hardware = RaspberryPi ] || [ $hardware = Tinkerboard ] || [ $hardware = OdroidXU4 ]; then
 	sudo rm gubiq-linux-arm-7
 	sudo rm /usr/bin/gubiq
-	wget https://github.com/ubiq/go-ubiq/releases/download/v3.1.0/gubiq-linux-arm-7
+	wget https://github.com/ubiq/go-ubiq/releases/download/v3.2.0/gubiq-linux-arm-7
   echo "f733349c34e466e30abf340e4ee677dd7c462df0b7c0bf0c75c9cd0dbb15faf1  gubiq-linux-arm-7" | sha256sum -c -
 
 elif [ $hardware = OdroidC2 ] || [ $hardware = LibreLePotato ]; then
 	sudo rm gubiq-linux-arm64
 	sudo rm /usr/bin/gubiq
-	wget https://github.com/ubiq/go-ubiq/releases/download/v3.1.0/gubiq-linux-arm64
+	wget https://github.com/ubiq/go-ubiq/releases/download/v3.2.0/gubiq-linux-arm64
   echo "5978700da6087fd78ffe913d90c48530e3d5f7f7927653020263b12649308194 gubiq-linux-arm64" | sha256sum -c -
 fi
 
