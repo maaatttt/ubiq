fab97e4f7d6a026027357bc6b0ccb2af4305ad75
matt <mmmaaatttttttt@gmail.com>
2020-11-18 18:33:21 -0600

    Update node.sh

--- node.sh
+++ node.sh
@@ -212,11 +212,11 @@ echo
 #### The binary file's checksum will be validated.  If it is valid, the script will complete the setup. If it is invalid, it will exit setup.
 
 if [ $arch = 32bit ]; then
-        wget https://github.com/ubiq/go-ubiq/releases/download/v3.1.0/gubiq-linux-arm-7
+        wget https://github.com/ubiq/go-ubiq/releases/download/v3.2.0/gubiq-linux-arm-7
         echo "f733349c34e466e30abf340e4ee677dd7c462df0b7c0bf0c75c9cd0dbb15faf1  gubiq-linux-arm-7" | sha256sum -c - || exit 1
         sudo cp ./gubiq-linux-arm-7 /usr/bin/gubiq
 elif [ $arch = 64bit ]; then
-        wget https://github.com/ubiq/go-ubiq/releases/download/v3.1.0/gubiq-linux-arm64
+        wget https://github.com/ubiq/go-ubiq/releases/download/v3.2.0/gubiq-linux-arm64
         echo "5978700da6087fd78ffe913d90c48530e3d5f7f7927653020263b12649308194 gubiq-linux-arm64" | sha256sum -c - || exit 1
         sudo cp ./gubiq-linux-arm64 /usr/bin/gubiq
 fi
