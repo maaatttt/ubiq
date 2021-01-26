e9012235e0450eaadc795e549bbbce7a092fb224
maaatttt <mmmaaatttttttt@gmail.com>
2021-01-26 13:29:10 -0600

    Added support for non-pi boards.

--- ubiq-config.sh
+++ ubiq-config.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 node_ip=$(hostname -I|cut -d" " -f 1)
 
+if grep -q 'Raspberry' /proc/device-tree/model; then
+        hardware=RaspberryPi
+        arch=32bit
+elif grep -q 'Tinker' /proc/device-tree/model; then
+        hardware=Tinkerboard
+        arch=32bit
+elif grep -q 'XU4' /proc/device-tree/model; then
+        hardware=OdroidXU4
+        arch=32bit
+elif grep -q 'ODROID-C2' /proc/device-tree/model; then
+        hardware=OdroidC2
+        arch=64bit
+elif grep -q 'Libre' /proc/device-tree/model; then
+        hardware=LibreLePotato
+        arch=64bit
+fi
+
+if [ $hardware = RaspberryPi ]; then
+        rootuser=pi
+elif [ $hardware != RaspberryPi ]; then
+        rootuser=root
+fi
+
 whiptail \
     --title "Welcome to Ubiq-Config" \
     --msgbox "This tool provides options for creating and managing a node for the Ubiq network, as well as helpful tools such as system updates and installing the Shinobi interface." --ok-button Continue 10 49
@@ -19,8 +42,8 @@ function advancedMenu() {
         1)
             if [ -f "/usr/bin/gubiq" ]; then
             whiptail --title "Ubiq Node Install" --msgbox "gubiq is already installed on your system" 8 45
-            elif [ "$(whoami)" != "pi" ]; then
-            whiptail --title "Block Info" --msgbox "Please log in as 'pi' and run 'ubiq-config' again in order to perform this action." 8 47
+            elif [ "$(whoami)" != $rootuser ]; then
+            whiptail --title "Block Info" --msgbox "Please log in as "$rootuser" and run 'ubiq-config' again in order to perform this action." 8 47
             elif (whiptail --title "Ubiq Node Install" --yesno "Would you like to begin installation of your Ubiq node now?" 8 64); then
                         if( whiptail --title "Timezone" --msgbox "Please set the correct timezone for your location." 8 54); then
                         sudo dpkg-reconfigure tzdata
@@ -35,7 +58,7 @@ function advancedMenu() {
         ;;
         2)
             if [ -f "/usr/bin/gubiq" ]; then
-                if [ "$(whoami)" != "node" ]; then
+                if [ "$(whoami)" = $rootuser ]; then
                 whiptail --title "Block Info" --msgbox "Please log in as 'node' and run 'ubiq-config' again in order to perform this action." 8 50
                 elif ( whiptail --title "Block Info" --yesno "Would you like to display a live view of block information?" 8 64); then
                 whiptail --title "Block Info" --msgbox "When you are finished, pressing 'Ctrl C' then 'q' will escape back to 'ubiq-config'" 8 46
@@ -47,8 +70,8 @@ function advancedMenu() {
         ;;
         3)
             if [ -f "/home/pi/auto.sh" ]; then
-                if [ "$(whoami)" != "pi" ] ; then
-                whiptail --title "gubiq Update" --msgbox "Please log in as 'pi' and run 'ubiq-config' again in order to perform this action." 8 47
+                if [ "$(whoami)" != $rootuser ] ; then
+                whiptail --title "gubiq Update" --msgbox "Please log in as "$rootuser" and run 'ubiq-config' again in order to perform this action." 8 47
                 elif ( whiptail --title "gubiq Update" --yesno "Your system is already configured for monthly automatic updates.  Would you like to update now anyway?" 8 60); then
                 wget https://raw.githubusercontent.com/maaatttt/ubiq/master/gu.sh
                 #chmod +x gu.sh
@@ -56,7 +79,7 @@ function advancedMenu() {
                 else whiptail --title "gubiq Update" --msgbox "Update has been canceled." 8 29
                 fi
             elif [ -f "/usr/bin/gubiq" ]; then
-                if ( whiptail --title "gubiq Update" --yesno "Would you like to update gubiq now?" 8 40); then
+                if ( whiptail --title "gubiq Update" --yesno "Would you like to update gubiq now?" 8 39); then
                 wget https://raw.githubusercontent.com/maaatttt/ubiq/master/gu.sh
                 chmod +x gu.sh
                 ./gu.sh
@@ -68,7 +91,7 @@ function advancedMenu() {
         ;;
         4)
             if [ -f "/usr/bin/gubiq" ]; then
-                if [ "$(whoami)" != "node" ] ; then
+                if [ "$(whoami)" = $rootuser ] ; then
                 whiptail --title "Resync" --msgbox "Please log in as 'node' and run 'ubiq-config' again in order to perform this action." 8 50
                 elif ( whiptail --title "Resync" --yesno "Would you like to delete all gubiq data and sync the blockchain from scratch?" 8 43); then
                 sudo apt update
@@ -83,12 +106,18 @@ function advancedMenu() {
             fi
         ;;
         5)
-            if [ "$(whoami)" != "pi" ]; then
-            whiptail --title "Shinobi Interface Installation" --msgbox "Please log in as 'pi' and run 'ubiq-config' again to perform this action." 8 47
-            elif [ "$(whoami)" = "pi" ]; then
-                if [ -f "/home/pi/shinobi.sh" ]; then
-                whiptail --title "Shinobi Interface Installation" --msgbox "Shinobi Interface is already installed on your system.  Use Shinobi by visiting "$node_ip":8888 in a browser." 9 60
-                elif(whiptail --title "Shinobi Interface Installation" --yesno "Would you like to install Shinobi Interface now?" 8 52); then
+            if [ "$(whoami)" != $rootuser ]; then
+            whiptail --title "Shinobi Interface Installation" --msgbox "Please log in as "$rootuser" and run 'ubiq-config' again to perform this action." 8 47
+            elif [ "$(whoami)" = $rootuser ]; then
+                if [ $hardware = "RaspberryPi" ]; then
+                        if [ -f "/home/pi/shinobi.sh" ]; then
+                        whiptail --title "Shinobi Interface Installation" --msgbox "Shinobi Interface is already installed on your system.  Use Shinobi by visiting "$node_ip":8888 in a browser." 9 60
+                        fi
+                elif [ $hardware != "RaspberryPi" ]; then
+                        if [ -f "/root/shinobi.sh" ]; then
+                        whiptail --title "Shinobi Interface Installation" --msgbox "Shinobi Interface is already installed on your system.  Use Shinobi by visiting "$node_ip":8888 in a browser" 9 60
+                        fi
+            elif(whiptail --title "Shinobi Interface Installation" --yesno "Would you like to install Shinobi Interface now?" 8 52); then
                 clear
                 wget https://raw.githubusercontent.com/maaatttt/ubiq/master/shinobi.sh
                 sudo chmod +x shinobi.sh
