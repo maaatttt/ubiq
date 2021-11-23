923651b15847b36bc518a88de20f6c176cb955ea
matt <mmmaaatttttttt@gmail.com>
2021-11-23 14:06:45 -0600

    Update node.sh
    Update gubiq to v 5.3.1 and update related shasums

--- node.sh
+++ node.sh
@@ -164,13 +164,13 @@ fi
 #### The binary file's checksum will be validated.  If it is valid, the script will complete the setup. If it is invalid, it will exit setup.
 
 if [ $arch = 32bit ]; then
-        wget https://github.com/ubiq/go-ubiq/releases/download/v5.3.0/gubiq-linux-arm7
-        echo "4373f9ace456325290887af581b3db53211797123758b84240fa629358395022 gubiq-linux-arm7" | sha256sum -c - || exit 1
+        wget https://github.com/ubiq/go-ubiq/releases/download/v5.3.1/gubiq-linux-arm7
+        echo "9ba60a7091feeaf8b8115046130ddc3b770cad74c3b97e83b46edfc57e34a088 gubiq-linux-arm7" | sha256sum -c - || exit 1
         sudo cp ./gubiq-linux-arm7 /usr/bin/gubiq
         sudo chmod +x /usr/bin/gubiq
 elif [ $arch = 64bit ]; then
-        wget https://github.com/ubiq/go-ubiq/releases/download/v5.3.0/gubiq-linux-arm64
-        echo "a7b54a1bf451b9e66cc4676bcf574b1c35841d8689e70bb789e33917ddc3d1b6 gubiq-linux-arm64" | sha256sum -c - || exit 1
+        wget https://github.com/ubiq/go-ubiq/releases/download/v5.3.1/gubiq-linux-arm64
+        echo "d2ab0fe4a99ff2888eece492dc39314ebae8f4007c69d0cafe8314fbe8c185c0 gubiq-linux-arm64" | sha256sum -c - || exit 1
         sudo cp ./gubiq-linux-arm64 /usr/bin/gubiq
         sudo chmod +x /usr/bin/gubiq
 fi
